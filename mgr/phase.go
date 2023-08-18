3f65ad3e2096c1ab31553ab7cc3426cfecb0e676
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-08-17 17:56:38 -0700

    0.0.2: added mgr

---
+++ mgr/phase.go
@@ -0,0 +1,28 @@
+package mgr
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/dattaray-basab/cks-ckip-lib/common"
+)
+
+func CreatePhase(
+
+	templaterDirpath string,
+	phaseName string,
+	codeBlock string,
+	dependentPhaseNames string) {
+
+	fmt.Println("")
+	fmt.Println("<<< CreatePhase/n")
+
+	recipePath := common.GetRecipePath(templaterDirpath, false)
+	fmt.Println("templaterDirpath::", templaterDirpath)
+	fmt.Println("recipePath::", recipePath)
+	if templaterDirpath != recipePath && !strings.HasPrefix(templaterDirpath, recipePath) {
+		_ = fmt.Errorf("Recipe path %s is not a subdirectory of %s", templaterDirpath, recipePath)
+	}
+	fmt.Println(">>> CreatePhase")
+	fmt.Println()
+}
---
+++ mgr/recipe.go
@@ -0,0 +1,65 @@
+package mgr
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/dattaray-basab/cks-ckip-lib/common"
+)
+
+func CreateRecipe(absPathToSource string, absPathToRecipeParent string) string {
+	pathToRecipe := prolog(absPathToRecipeParent, absPathToSource)
+	// err := CreatePathIfAbsent(pathToRecipe)
+	// if err != nil {
+	// 	return err
+	// }
+	return pathToRecipe
+}
+
+func prolog(absPathToRecipeParent string, absPathToSource string) string {
+	fmt.Println()
+	fmt.Println("<<< CreateRecipe/n")
+	fmt.Println("absPathToRecipeParent::", absPathToRecipeParent)
+	fmt.Println("absPathToSource::", absPathToSource)
+	absPathToRecipe := filepath.Join(absPathToRecipeParent, "__recipe")
+	pathToRecipe := common.GetRecipePath(absPathToRecipe, true)
+	fmt.Println("pathToRecipe::", pathToRecipe)
+	fmt.Println(">>> CreateRecipe")
+	fmt.Println()
+	return pathToRecipe
+}
+
+func CreatePathIfAbsent(recipePath string) error {
+	if _, err := os.Stat(recipePath); errors.Is(err, os.ErrNotExist) {
+
+		fmt.Println("recipe folder already exists: Overwrite by entering y/Y: ")
+
+		// var then variable name then variable type
+		var doOverride string
+
+		// Taking input from user
+		fmt.Scanln(&doOverride)
+
+		if doOverride == "y" || doOverride == "Y" {
+			fmt.Println("Overwriting recipe folder")
+
+			err := os.Mkdir(recipePath, os.ModePerm)
+			if err != nil {
+				return err
+			}
+		} else {
+			fmt.Println("Exiting")
+			return errors.New("Exiting")
+		}
+	} else {
+		fmt.Println("recipe folder does not exist: Creating")
+		err := os.Mkdir(recipePath, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return nil
+}
