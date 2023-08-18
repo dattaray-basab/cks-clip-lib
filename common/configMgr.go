5e19d2d4d148ad4fd4d67f89ef4106dcd6e64388
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-08-17 17:54:06 -0700

    0.0.2: added globals and common

---
+++ common/configMgr.go
@@ -0,0 +1,18 @@
+package common
+
+import (
+	"github.com/dattaray-basab/cks-ckip-lib/globals"
+	"github.com/spf13/viper"
+)
+
+func GetRecipePath(absPathToRecipe string, reset bool) string {
+
+	viper.SetConfigFile("config.yaml")
+	if err := viper.ReadInConfig(); err != nil  || reset {
+		viper.Set(globals.RecipePathKey, absPathToRecipe)
+		viper.WriteConfig()
+	}
+	return viper.GetString(globals.RecipePathKey)
+}
+
+
---
+++ globals/constants.go
@@ -0,0 +1,6 @@
+package globals
+
+const RecipePathKey = "CKS_ENV_RECIPE_PATH"
+
+const RecipeDirectory = "__DEV_3"
+// const RecipeDirectory = "__recipe"
