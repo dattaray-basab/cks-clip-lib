71c45f49f4234295039c7553b130406496592608
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-08-25 12:32:53 -0700

    0.2.2: expt with writing files

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"bufio"
+	// "fmt"
+	"os"
+	// "strconv"
+)
+ 
 func main() {
-	println("cks-cliplib")
+	// lines := [2]string{"我想吃！", "I want to eat!"}
+
+	text := `Hi, 0 我想吃！
+	How are you?
+my name is basab`
+ 
+	// Create a file for writing
+	f, _ := os.Create("file2.txt")
+ 
+	// Create a writer
+	w := bufio.NewWriter(f)
+
+		w.WriteString(text)
+ 
+	// for i, s := range lines {
+	// 	fmt.Println(i)
+	// 	w.WriteString(strconv.Itoa(i) + " " + s + "\n")
+	// }
+ 
+	// Very important to invoke after writing a large number of lines
+	w.Flush()
 }
