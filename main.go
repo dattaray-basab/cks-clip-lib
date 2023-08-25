ddb4641f8493e25601b1a317c73cfabca0eff2ff
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-08-25 12:37:39 -0700

    0.2.2: expt with writing files

--- main.go
+++ main.go
@@ -13,20 +13,34 @@ func main() {
 	text := `Hi, 0 我想吃！
 	How are you?
 my name is basab`
+
+	file_name := "file2.txt"
  
 	// Create a file for writing
-	f, _ := os.Create("file2.txt")
- 
 	// Create a writer
-	w := bufio.NewWriter(f)
-
-		w.WriteString(text)
- 
 	// for i, s := range lines {
 	// 	fmt.Println(i)
 	// 	w.WriteString(strconv.Itoa(i) + " " + s + "\n")
 	// }
- 
 	// Very important to invoke after writing a large number of lines
+	err := createFile(file_name, text)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func createFile(file_name string, text string)  error {
+
+	f, err := os.Create(file_name)
+	if err != nil {
+		return err
+	}
+	w := bufio.NewWriter(f)
+
+	w.WriteString(text)
+
 	w.Flush()
+	f.Close()
+	return nil
 }
