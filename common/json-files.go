faf5ec9d7ca45f5f078dbd5d9e08bbe1b6800f26
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-08-27 19:31:45 -0700

    0.3.2: add alter

--- common/json-files.go
+++ common/json-files.go
@@ -1,13 +1,28 @@
 package common
 
-import "os"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
-func ReadJsonFile(filepath string) (map[string]interface{}, error) {
-	jsonMap := make(map[string]interface{})
-	jsonFile, err := os.Open(filepath)
-	if err != nil {
-		return jsonMap, err
-	}
-	defer jsonFile.Close()
-	return jsonMap, nil
+func ReadJsonFile(filePath string) (map[string]interface{}, error) {
+// Open our jsonFile
+    jsonFile, err := os.Open(filePath)
+    // if we os.Open returns an error then handle it
+    if err != nil {
+        return nil, err
+    }
+    fmt.Println("Successfully Opened users.json")    // defer the closing of our jsonFile so that we can parse it later on
+    defer jsonFile.Close()
+
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+
+    var result map[string]interface{}
+    json.Unmarshal([]byte(byteValue), &result)
+
+	return result, nil
+
+
 }
