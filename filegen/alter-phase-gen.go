7988d53e7171a380872256b7e1bafd088316045b
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-08-27 23:59:24 -0700

    0.3.3: add alter

--- filegen/alter-phase-gen.go
+++ filegen/alter-phase-gen.go
@@ -49,17 +49,36 @@ func CreateOrUpdatePhaseFile(phasePath, phaseName, lastPhase string) error {
 		return false, nil
 
 	}
-	var createPhaseFile = func(phasePath string, phaseName string) error {
-		phaseFilePath := filepath.Join(phasePath, phaseName+globals.JSON_EXT)
-		log.Println(phaseFilePath)
-		_, err := os.Create(phaseFilePath)
-		if err != nil {
-			return err
+	var createPhaseFile = func(phasePath string, phaseName, lastPhase string) error {
+		baseDirpath := filepath.Join(phasePath, phaseName+globals.JSON_EXT)
+		recipeScaffold := globals.ScaffoldInfoTListT{
+
+			{
+				Filepath: filepath.Join(baseDirpath),
+				Content: `
+{
+  "__DEPENDS_ON": [
+	{{last-phase}}
+  ],
+  "ops_pipeline": [
+	{
+	  {{alter-name}}: {
+		"locator": [
+		  {{alter-dir-path}}
+		]
+	  }
+	}
+  ]
+}
+		`,
+			},
 		}
-		return nil
+
+		err := common.CreateFiles(recipeScaffold)
+		return err
 	}
 
-	var updatePhaseFile = func(phasePath string, phaseName string) error {
+	var updatePhaseFile = func(phasePath string, phaseName, lastPhase string) error {
 		phaseFilePath := filepath.Join(phasePath, phaseName+globals.JSON_EXT)
 		log.Println(phaseFilePath)
 		_, err := os.Create(phaseFilePath)
@@ -80,23 +99,22 @@ func CreateOrUpdatePhaseFile(phasePath, phaseName, lastPhase string) error {
 	}
 
 	// does phase already exist?
-	currentPhaseDilePath := filepath.Join(phasePath, phaseName+globals.JSON_EXT)
-	isFile := common.IsFile(currentPhaseDilePath)
+	currentPhaseFilePath := filepath.Join(phasePath, phaseName+globals.JSON_EXT)
+	isFile := common.IsFile(currentPhaseFilePath)
 	log.Println(isFile)
 	if isFile {
 		// if so update the file
-		err = updatePhaseFile(phasePath, phaseName)
+		err = updatePhaseFile(phasePath, phaseName, lastPhase)
 		if err != nil {
 			return err
 		}
 	} else {
 		// if not create a new file
-		err = createPhaseFile(phasePath, phaseName)
+		err = createPhaseFile(phasePath, phaseName, lastPhase)
 		if err != nil {
 			return err
 		}
 	}
-
 
 	return nil
 }
