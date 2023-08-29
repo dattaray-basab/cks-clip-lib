93c17f7a0fe45ec94eaa4389536910825d0c2c84
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-08-28 21:20:33 -0700

    0.3.6: alter - creating conrol file

--- alter/alter-create.go
+++ alter/alter-create.go
@@ -2,17 +2,22 @@ package alter
 
 import (
 	// "log"
+
 	"log"
+	"os"
 	"path/filepath"
+	"strings"
 
 	// "strings"
 
 	"github.com/dattaray-basab/cks-clip-lib/common"
 	"github.com/dattaray-basab/cks-clip-lib/globals"
+
 )
 
 var CreatePhaseFile = func(templateMap map[string]string) error {
 	var buildNewPhaseFile = func(phasePath string, phaseName, lastPhase string) error {
+
 		baseDirpath := filepath.Join(phasePath, phaseName+globals.JSON_EXT)
 		scaffold := globals.ScaffoldInfoTListT{
 
@@ -42,25 +47,79 @@ var CreatePhaseFile = func(templateMap map[string]string) error {
 	}
 
 	var buildAlterDir = func(templateMap map[string]string) error {
+
 		templateMap[globals.KEY_STORE_DIR_PATH] = filepath.Join(templateMap[globals.KEY_ALTER_PATH], globals.STORE_DIRNAME)
 		templateMap[globals.KEY_CONTROL_JSON_PATH] = filepath.Join(templateMap[globals.KEY_ALTER_PATH], globals.CONTROL_JSON_FILE)
 
-		log.Println("templateMap[globals.KEY_STORE_DIR_PATH]: ", templateMap[globals.KEY_STORE_DIR_PATH])
-		scaffold := globals.ScaffoldInfoTListT{
+		var buildControlFile = func(templateMap map[string]string) error {
+			scaffold := globals.ScaffoldInfoTListT{
 
-			{
-				Filepath: filepath.Join(templateMap[globals.KEY_CONTROL_JSON_PATH]),
-				Content: `
-[
-  {
-	"op": "transform"
-  }
-]
-		`,
-			},
+				{
+					Filepath: filepath.Join(templateMap[globals.KEY_CONTROL_JSON_PATH]),
+					Content: `
+	[
+	{
+		"op": "transform"
+	}
+	]
+			`,
+				},
+			}
+
+			err := common.CreateFiles(scaffold)
+			if err != nil {
+				return err
+			}
+
+			return nil
 		}
 
-		err := common.CreateFiles(scaffold)
+		var buildStoreDir = func(templateMap map[string]string) error {
+
+			var matches = func(list []string, item string) bool {
+				for _, listItem := range list {
+					if listItem == item {
+						return true
+					}
+				}
+				return false
+			}
+
+			move_items := strings.Split(templateMap[globals.KEY_MOVE_ITEMS], ":")
+			log.Println(move_items)
+
+			files, err := os.ReadDir(templateMap[globals.KEY_CODE_BLOCK_PATH])
+			if err != nil {
+				return err
+			}
+			for _, item := range files {
+				if matches(move_items, item.Name()) {
+					item_path := filepath.Join(templateMap[globals.KEY_CODE_BLOCK_PATH], item.Name())
+					if common.IsDir(item_path) {
+						parentDir := filepath.Dir(item_path)
+						newParent := filepath.Join(parentDir, globals.STORE_DIRNAME)		
+						log.Println(newParent)
+						err := os.MkdirAll(newParent, os.ModePerm)
+						if err != nil {
+							return err
+						}
+						new_dirpath := filepath.Join(newParent, item.Name())
+						err = os.Rename(item_path, new_dirpath)
+						if err != nil {
+							return err
+						}
+					} 
+				}
+			}
+			return nil
+		}
+
+		err := buildStoreDir(templateMap)
+		if err != nil {
+			return err
+		}
+
+		err = buildControlFile(templateMap)
 		if err != nil {
 			return err
 		}
