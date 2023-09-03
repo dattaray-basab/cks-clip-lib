664e57a8df5385f889363480b321df869e6252f0
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-09-02 20:31:29 -0700

    0.4.0: add alter - alter-update DONE

--- alter/alter-update.go
+++ alter/alter-update.go
@@ -51,12 +51,12 @@ var UpdatePhaseFile = func(templateMap map[string]string) error {
 	}
 
 	var getAlterJson = func(templateMap map[string]string) map[string]interface{} {
-		fullAlterPathWithQuotes:= templateMap[globals.KEY_FULL_ALTER_PATH_WITH_QUOTES]
+		fullAlterPathWithQuotes := templateMap[globals.KEY_FULL_ALTER_PATH_WITH_QUOTES]
 
 		jsonStr := `	{
 	  "alter": {
 		"locator": [` +
-		  fullAlterPathWithQuotes + `
+			fullAlterPathWithQuotes + `
 		]
 	  }
 	}`
@@ -67,52 +67,64 @@ var UpdatePhaseFile = func(templateMap map[string]string) error {
 		return jsonFrame
 	}
 
+	// check if the alter command locator is already present
 	var checkForDuplicateAlter = func(opsPipeline []interface{}, alterCommand interface{}) error {
-		// check if the alter command is already present
 		for _, alterCommandInPipeline := range opsPipeline {
 			pathFromPipeline := alterCommandInPipeline.(map[string]interface{})["alter"].(map[string]interface{})["locator"].([]interface{})[0].(string)
 			pathFromAlterCommand := alterCommand.(map[string]interface{})["alter"].(map[string]interface{})["locator"].([]interface{})[0].(string)
-			// alterCommandInPipelineStr := fmt.Sprintf("%v", alterCommandInPipeline)
-			// alterCommandStr := fmt.Sprintf("%v", alterCommand)
 			if pathFromPipeline == pathFromAlterCommand {
 				return errors.New("alter command already present")
 			}
 		}
 		return nil
-
 	}
 
-	phaseContent, err := getPhaseData(templateMap)
+	var addNewAlterToPhase = func(templateMap map[string]string) (map[string] interface{}, error) {
+		phaseContent, err := getPhaseData(templateMap)
+		if err != nil {
+			return nil, err
+		}
+
+		alterCommand := getAlterJson(templateMap)
+		logger.Log.Debug("alter command to add: ", alterCommand)
+
+		opsPipeline := phaseContent["ops_pipeline"].([]interface{})
+		err = checkForDuplicateAlter(opsPipeline, alterCommand)
+		if err != nil {
+			return nil, err
+		}
+
+		appendedPipeline := append(opsPipeline, alterCommand)
+		phaseContent["ops_pipeline"] = appendedPipeline
+		return phaseContent, nil
+	}
+
+	var writePhaseFile = func(phaseContent map[string]interface{}) error {
+		jsonifiedPhase, _ := json.MarshalIndent(phaseContent, "", "  ")
+		logger.Log.Debug(string(jsonifiedPhase))
+
+		// fmt.Println(string(jsonifiedPhase))
+		phaseName := templateMap[globals.KEY_PHASE_NAME]
+		phasesPath := templateMap[globals.KEY_PHASES_PATH]
+		fullPhasePath := filepath.Join(phasesPath, phaseName+globals.JSON_EXT)
+
+		err := os.WriteFile(fullPhasePath, jsonifiedPhase, fs.ModeAppend.Perm())
+		if err != nil {
+			msg := fmt.Sprintf("phase file could not be written to: %s", fullPhasePath)
+			err = errors.New(msg)
+			return err
+		}
+		return nil
+	}
+
+	phaseContent, err := addNewAlterToPhase(templateMap)
 	if err != nil {
 		return err
 	}
 
-	alterCommand := getAlterJson(templateMap)
-	logger.Log.Debug("alter command to add: ", alterCommand)
-
-	opsPipeline := phaseContent["ops_pipeline"].([]interface{})
-	err = checkForDuplicateAlter(opsPipeline, alterCommand)
+	err = writePhaseFile(phaseContent)
 	if err != nil {
 		return err
 	}
-	
-	appendedPipeline := append(opsPipeline, alterCommand)
-	phaseContent["ops_pipeline"] = appendedPipeline
-
-	jsonifiedPhase, _ := json.MarshalIndent(phaseContent, "", "  ")
-	logger.Log.Debug(string(jsonifiedPhase))
-
-	fmt.Println(string(jsonifiedPhase))
-	phaseName := templateMap[globals.KEY_PHASE_NAME]
-	phasesPath := templateMap[globals.KEY_PHASES_PATH]
-	fullPhasePath := filepath.Join(phasesPath, phaseName+globals.JSON_EXT)
-
-	err = os.WriteFile(fullPhasePath, jsonifiedPhase, fs.ModeAppend.Perm())
-	if err != nil {
-		msg := fmt.Sprintf("phase file could not be written: %s", err.Error())
-		err = errors.New(msg)
-		return err
-	}
-
 	return nil
 }
