278d23b67410513e611c7889aef9e615c744e1de
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-09-07 00:37:52 -0700

    0.4.6: transform DONE

--- alter/new-code.go
+++ alter/new-code.go
@@ -6,6 +6,8 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/dattaray-basab/cks-clip-lib/alter/pick"
+	"github.com/dattaray-basab/cks-clip-lib/alter/recast"
 	"github.com/dattaray-basab/cks-clip-lib/alter/transform"
 	"github.com/dattaray-basab/cks-clip-lib/globals"
 	"github.com/dattaray-basab/cks-clip-lib/logger"
@@ -72,9 +74,26 @@ var BuildNewAlterDir = func(templateMap map[string]string) error {
 		return err
 	}
 
-	err = transform.BuildSubcommand(templateMap)
-	if err != nil {
-		return err
+	switch templateMap[globals.KEY_ALTER_SUB_COMMAND] {
+	case globals.TRANSFORM:
+		err = transform.BuildSubcommand(templateMap)
+		if err != nil {
+			return err
+		}
+	case globals.RECAST:
+		err = recast.BuildSubcommand(templateMap)
+		if err != nil {
+			return err
+		}
+	case globals.PICK:
+		err = pick.BuildSubcommand(templateMap)
+		if err != nil {
+			return err
+		}
+	default:
+		msg := "*** FAILED ***: unknown alter sub-command"
+		logger.Log.Error(msg)
+		return errors.New(msg)
 	}
 
 	return nil
