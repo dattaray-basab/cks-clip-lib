a27f22888f1b90967df51b433b2e8023c595589d
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-09-06 23:34:12 -0700

    0.4.5.2: recast

---
+++ alter/recast/subcommand.go
@@ -0,0 +1,31 @@
+package recast
+
+import (
+	"path/filepath"
+
+	"github.com/dattaray-basab/cks-clip-lib/common"
+	"github.com/dattaray-basab/cks-clip-lib/globals"
+)
+
+var BuildSubcommand = func(templateMap map[string]string) error {
+	scaffold := globals.ScaffoldInfoTListT{
+
+		{
+			Filepath: filepath.Join(templateMap[globals.KEY_CONTROL_JSON_PATH]),
+			Content: `
+	[
+	{
+		"op": "transform"
+	}
+	]
+			`,
+		},
+	}
+
+	err := common.CreateFiles(scaffold)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
