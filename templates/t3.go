9bac2dc36d9b4df4d0c87f8a4ee41569479eb3b8
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-09-09 11:25:21 -0700

    0.4.9: pick - expt with 3 go templates

--- templates/t3.go
+++ templates/t3.go
@@ -3,8 +3,8 @@ var (
   T3 = `
 {{- range $name, $map := . -}}
 {{ $name }}:
-  {{- range $name, $values := $map }}
-  {{ $name }}:
+  {{- range $name2, $values := $map }}
+  {{ $name2 }}:
     {{- range $value := $values }}
     {{ $value }}
     {{- end }}
