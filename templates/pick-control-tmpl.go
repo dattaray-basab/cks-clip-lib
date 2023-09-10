a4e5586906b74eb7d6a8762a004f70cfbf78a022
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-09-10 13:17:42 -0700

    0.4.9: pick - separating exptTemplateRunner from TemplateRunner

--- templates/pick-control-tmpl.go
+++ templates/pick-control-tmpl.go
@@ -3,23 +3,23 @@ var (
   PickQueryTemplate = `
 {
   "__CONTENT": [
-
       {
           "id": {{.ShortQueryId}},
           "kind": "multiselect",
           "prompt": "enter ...",
           "selector": [
-        {{ range $k, $v := .MoveItemsInfo -}}
-            {{ $v.Index }}
-            {{- if $v.IsLastItem -}}{{else}},{{- end }}
-        {{- end }}
+        {{- range $k, $v := .MoveItemsInfo }}
+            {{- $v.Index }}
+            {{- if $v.IsLastItem }}{{ else }}, {{ end -}}
+        {{- end -}}
           ],
           "children": {
             "kind": "literal",
               "value": [
-        {{ range $k, $v := .MoveItemsInfo -}}
-                  {{ $v.Key }},
-        {{- end }}
+        {{- range $k, $v := .MoveItemsInfo }}
+            {{- $v.Key }}
+            {{- if $v.IsLastItem }}{{ else }}, {{ end -}}
+        {{- end -}}
               ]
             },
       }
