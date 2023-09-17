347d4417ce37274b684c41437029f73bec1bae26
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-09-16 17:19:45 -0700

    0.5.5: templates being stored with the commands

--- alter/transform/subcommand.go
+++ alter/transform/subcommand.go
@@ -1,31 +1,10 @@
 package transform
 
 import (
-	"path/filepath"
-
 	"github.com/dattaray-basab/cks-clip-lib/common"
-	"github.com/dattaray-basab/cks-clip-lib/globals"
 )
 
 var BuildSubcommand = func(templateMap map[string]string) error {
-	scaffold := globals.ScaffoldInfoTListT{
-
-		{
-			Filepath: filepath.Join(templateMap[globals.KEY_CONTROL_JSON_PATH]),
-			Content: `
-[
-	  {
-		"op": "transform"
-	  }
-]
-`,
-		},
-	}
-
-	err := common.CreateFiles(scaffold)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	err := common.BuildAlterInfrastucture(templateMap, TransformQueryTemplate, TransformControlTemplate)
+	return err
 }
