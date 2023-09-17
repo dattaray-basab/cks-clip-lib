942607ca2b2e7a3e43601c4bb9a35c1880d0f765
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-09-17 02:14:16 -0700

    0.5..?: transform WIP

--- alter/transform/subcommand.go
+++ alter/transform/subcommand.go
@@ -1,6 +1,7 @@
 package transform
 
 import (
+	"bufio"
 	"fmt"
 	"io/fs"
 	"io/ioutil"
@@ -14,6 +15,31 @@ import (
 )
 
 var BuildSubcommand = func(templateMap map[string]string) error {
+
+	var getWordsFromFile = func(filePath string) []string {
+		file, err := os.Open(filePath)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+
+		// Set the split function for the scanning operation.
+		scanner.Split(bufio.ScanWords)
+
+		// Scan all words from the file.
+		var words []string
+		for scanner.Scan() {
+			// fmt.Println(scanner.Text())
+			words = append(words, scanner.Text())
+		}
+
+		if err := scanner.Err(); err != nil {
+			panic(err)
+		}
+		return words
+	}
 
 	var getFirstLineOfFirstFile = func(templateMap map[string]string) (string, error) {
 		var getFirstFile = func(filePath string) (string, error) {
@@ -47,18 +73,23 @@ var BuildSubcommand = func(templateMap map[string]string) error {
 			return firstLine, nil
 		}
 
-
 		storePath := filepath.Join(templateMap[globals.KEY_ALTER_PATH], globals.STORE_DIRNAME)
 		if !common.IsDir(storePath) {
 			err := fmt.Errorf("store-path %s does not exist", storePath)
 			return "", err
 		}
 
+
 		firstFile, err := getFirstFile(storePath)
 		if err != nil {
 			return "", err
 		}
 		logger.Log.Debug(firstFile)
+
+		wordList := getWordsFromFile(firstFile)
+		logger.Log.Debug(wordList[0])
+
+
 
 		firstLine, err := readFirstLine(firstFile)
 		if err != nil {
