7446f91ea08282fae9576ac211fa99306aaabd6a
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-09-17 13:37:28 -0700

    0.5.8: transform - introduced FilePrepend

--- main.go
+++ main.go
@@ -1,42 +1,13 @@
 package main
 
-import (
-	"bufio"
-	"path/filepath"
-	// "fmt"
-	"os"
-	// "strconv"
-)
- 
+import "github.com/dattaray-basab/cks-clip-lib/common"
+
+
+
+
 func main() {
-	// lines := [2]string{"我想吃！", "I want to eat!"}
-
-	text := `Hi, 0 我想吃！
-	How are you?
-my name is basab`
-
-	file_name := "file2.txt"
- 
-	err := createFile(file_name, text)
-	if err != nil {
-		panic(err)
-	}
-
-}
-
-func createFile(fileName string, text string)  error {
-
-	filePath := filepath.Join(fileName) 
-
-	f, err := os.Create(filePath)
-	if err != nil {
-		return err
-	}
-	w := bufio.NewWriter(f)
-
-	w.WriteString(text)
-
-	w.Flush()
-	f.Close()
-	return nil
+		err := common.PrependToFile("test.txt", "bye")
+		if err != nil {
+			println(err.Error())
+		}
 }
