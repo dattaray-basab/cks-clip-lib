f4bc62c1aef41a240e92914440d0bcf7e07e4fad
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-09-17 13:37:42 -0700

    0.5.8: transform - introduced FilePrepend

---
+++ common/FilePrepend.go
@@ -0,0 +1,77 @@
+package common
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Record struct {
+	Filename string
+	Contents []string
+}
+
+var NewRecord = func(filename string) *Record {
+	return &Record{
+		Filename: filename,
+		Contents: make([]string, 0),
+	}
+}
+
+func (r *Record) readLines() error {
+	if _, err := os.Stat(r.Filename); err != nil {
+		return nil
+	}
+
+	f, err := os.OpenFile(r.Filename, os.O_RDONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if tmp := scanner.Text(); len(tmp) != 0 {
+			r.Contents = append(r.Contents, tmp)
+		}
+	}
+
+	return nil
+}
+
+func (r *Record) Prepend(content string) error {
+	err := r.readLines()
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(r.Filename, os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	writer := bufio.NewWriter(f)
+	writer.WriteString(fmt.Sprintf("%s\n", content))
+	for _, line := range r.Contents {
+		_, err := writer.WriteString(fmt.Sprintf("%s\n", line))
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := writer.Flush(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var PrependToFile = func(filename, content string) error {
+	record := NewRecord(filename)
+	err := record.Prepend(content)
+	if err != nil {
+		return err
+	}
+	return nil
+}
